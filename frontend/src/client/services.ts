8fc51ff5228bf4cbb7fab18d103f93b002efd3fc
Saif Husain Ansari <saifhussainansari6@gmail.com>
2024-12-01 01:17:34 +0530

    changes

--- frontend/src/client/services.ts
+++ frontend/src/client/services.ts
@@ -2,7 +2,7 @@ import type { CancelablePromise } from './core/CancelablePromise';
 import { OpenAPI } from './core/OpenAPI';
 import { request as __request } from './core/request';
 
-import type { TodoCreate, TodoRead, TodoUpdateSchema, PatientCreate, PatientRead, PatientUpdate } from './models';
+import type { TodoCreate, TodoRead, TodoUpdateSchema } from './models';
 
 export type TodoData = {
 	CreateTodoApiV1TodoPost: {
@@ -20,26 +20,6 @@ export type TodoData = {
 	};
 	DeleteTodoApiV1TodoTodoIdDelete: {
 		todoId: number
-
-	};
-}
-
-export type PatientData = {
-	CreatePatientApiV1PatientPost: {
-		requestBody: PatientCreate
-
-	};
-	GetPatientApiV1PatientIdGet: {
-		patientId: number
-
-	};
-	UpdatePatientApiV1PatientIdPut: {
-		patientId: number
-		requestBody: PatientUpdate
-
-	};
-	DeletePatientApiV1PatientIdDelete: {
-		patientId: number
 
 	};
 }
@@ -128,96 +108,6 @@ export class TodoService {
 			url: '/api/v1/todo/{todo_id}',
 			path: {
 				todo_id: todoId
-			},
-		});
-	}
-
-}
-
-export class PatientService {
-
-	/**
-	 * List All Patient
-	 * @returns PatientRead Successful Response
-	 * @throws ApiError
-	 */
-	public static listAllPatientApiV1PatientGet(): CancelablePromise<Array<PatientRead>> {
-		return __request(OpenAPI, {
-			method: 'GET',
-			url: '/api/v1/patient/',
-		});
-	}
-
-	/**
-	 * Create Patient
-	 * @returns PatientRead Successful Response
-	 * @throws ApiError
-	 */
-	public static createPatientApiV1PatientPost(data: PatientData['CreatePatientApiV1PatientPost']): CancelablePromise<PatientRead> {
-		const {
-			requestBody,
-		} = data;
-		return __request(OpenAPI, {
-			method: 'POST',
-			url: '/api/v1/patient/',
-			body: requestBody,
-			mediaType: 'application/json',
-		});
-	}
-
-	/**
-	 * Get a single Patient
-	 * @returns PatientRead Successful Response
-	 * @throws ApiError
-	 */
-	public static getPatientApiV1PatientIdGet(data: PatientData['GetPatientApiV1PatientIdGet']): CancelablePromise<PatientRead> {
-		const {
-			patientId,
-		} = data;
-		return __request(OpenAPI, {
-			method: 'GET',
-			url: '/api/v1/patient/{patient_id}',
-			path: {
-				patient_id: patientId
-			},
-		});
-	}
-
-	/**
-	 * Update a Patient
-	 * @returns PatientRead Successful Response
-	 * @throws ApiError
-	 */
-	public static updatePatientApiV1PatientIdPut(data: PatientData['UpdatePatientApiV1PatientIdPut']): CancelablePromise<PatientRead> {
-		const {
-			patientId,
-			requestBody,
-		} = data;
-		return __request(OpenAPI, {
-			method: 'PUT',
-			url: '/api/v1/patient/{patient_id}',
-			path: {
-				patient_id: patientId
-			},
-			body: requestBody,
-			mediaType: 'application/json',
-		});
-	}
-
-	/**
-	 * Delete a Patient
-	 * @returns void No Content
-	 * @throws ApiError
-	 */
-	public static deletePatientApiV1PatientIdDelete(data: PatientData['DeletePatientApiV1PatientIdDelete']): CancelablePromise<void> {
-		const {
-			patientId,
-		} = data;
-		return __request(OpenAPI, {
-			method: 'DELETE',
-			url: '/api/v1/patient/{patient_id}',
-			path: {
-				patient_id: patientId
 			},
 		});
 	}
