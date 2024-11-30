0ba7e829ff336c7e83349ed85c873f87861db483
Saif Husain Ansari <saifhussainansari6@gmail.com>
2024-12-01 00:30:51 +0530

    changes

--- frontend/src/components/AddTodoForm.tsx
+++ frontend/src/components/AddTodoForm.tsx
@@ -22,19 +22,19 @@ const AddTodoForm: React.FC<AddTodoFormProps> = ({ onAdd }) => {
 
   return (
     <form onSubmit={handleSubmit} className="mt-4">
-      <div className="flex items-center pd-5 m-5">
-        <input
+      <div className="flex flex-col pd-5 m-5 space-y-4">
+      <input
           type="text"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
           placeholder="Add a new todo..."
-          className="flex-grow px-4 py-2 text-gray-700 bg-gray-100 rounded-l-lg focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-300 ease-in-out"
+          className="w-full px-4 py-2 text-gray-700 bg-gray-100 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-300 ease-in-out"
         />
        
       </div>
       <button
           type="submit"
-          className="px-4 py-2 bg-blue-500 text-white rounded-r-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-300 ease-in-out"
+          className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-300 ease-in-out"
         >
           Add
         </button>
