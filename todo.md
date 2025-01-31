2eb51b3d9c97981d767eb2f46a3fb07d6c8965a9
Saif Husain Ansari <saifhussainansari6@gmail.com>
2025-02-01 02:32:59 +0530

    get figma code for download project

--- todo.md
+++ todo.md
@@ -2,6 +2,6 @@ integrate in our code
     prompt for fix app.jsx file {cm:2025-01-31}
     poc for add generated file in app.jsx file and place in components {cm:2025-01-31}
         fix if get error {cm:2025-01-31}
-    create a new branch to do further poc   
-        create an new file in the engine 
+    create a new branch to do further poc   {cm:2025-02-01}
+        create an new file in the engine
             then write code there get code from figma_code folder and place it in appropriate places
--- web_automation/main.py
+++ web_automation/main.py
@@ -1,4 +1,5 @@
 import os
+import traceback
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
@@ -9,147 +10,157 @@ from selenium.webdriver.common.action_chains import ActionChains
 from dotenv import load_dotenv
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+def wait_element(driver, by, value, timeout=10):
+    return WebDriverWait(driver, timeout).until(EC.element_to_be_clickable((by, value)))
 
+def wait_for_script(driver, script, retries=10):
+    def run_script(count=0):
+        try:
+            print(script)
+            driver.execute_script(script)
+        except Exception as e:
+            if count < retries:
+                time.sleep(0.5)
+                run_script(count + 1)
+            else:
+                raise Exception(e)
+    run_script()
 def automate_locofyai():
     try:
         load_dotenv()
 
         driver = webdriver.Chrome()
         driver.maximize_window()
-
         driver.get("https://www.figma.com/login")
-        time.sleep(5)
-        
+
+
         LOCOFY_EMAIL = os.getenv('LOCOFY_EMAIL')
         LOCOFY_PASSWORD = os.getenv('LOCOFY_PASSWORD')
-        
-        
+
+
         FIGMA_EMAIL = os.getenv('FIGMA_EMAIL')
         FIGMA_PASSWORD = os.getenv('FIGMA_PASSWORD')
-        
+
         FIGMA_FILE_URL = os.getenv('FIGMA_FILE_URL')
 
         # Enter email
-        email_field = driver.find_element(By.ID, "email")
+        email_field = wait_element(driver, By.ID, "email")
         email_field.send_keys(FIGMA_EMAIL)
+        email_field.send_keys(Keys.RETURN)
 
         # Enter password
-        password_field = driver.find_element(By.ID, "current-password")
+        password_field = wait_element(driver, By.ID, "current-password")
         password_field.send_keys(FIGMA_PASSWORD)
         password_field.send_keys(Keys.RETURN)
-        time.sleep(10)  # Wait for login to complete
+
+        wait_element(driver, By.CLASS_NAME,'tab_group--tabSelectorOverrides--misNs')
 
         # Step 2: Open Figma File
         driver.get(FIGMA_FILE_URL)
-        time.sleep(10)  # Wait for the file to load
-
+        # check element with text "Search"
+        wait_element(driver, By.CLASS_NAME,'gpu-view-content')
+        time.sleep(0.5)
         # Step 3: Open the Locofy.ai Plugin
         # Simulate pressing `Alt` + `/` to open the plugin search
         ActionChains(driver).key_down(Keys.COMMAND).send_keys("/").key_up(Keys.COMMAND).perform()
-        time.sleep(2)
-
         # Search for Locofy.ai
-        search_field = driver.find_element(By.XPATH, "//input[@placeholder='Search']")
+        search_field = wait_element(driver, By.XPATH, "//input[@placeholder='Search']")
         search_field.send_keys("Locofy.ai")
-        time.sleep(2)
-
+        time.sleep(0.5)
         # press enter
-        driver.execute_script('document.querySelector("#react-page > div > div > div > div.fullscreen_view--flexContainer--SBt2E > div:nth-child(1) > div > div > div.utils-module--contents--iP8Hi > div > div > div > div > div > div > div > div.cx_pl8--gxoGN.cx_pr8--CsQT3.cx_pb8--v4HS8 > div > div > fieldset > button:nth-child(3) > div > div.tabs--tabButton--QIg8n > label > i18n-text").click()')
-        time.sleep(2)
+        search_field.send_keys(Keys.RETURN)
+        time.sleep(1.5)
+        # press enter
+        search_field.send_keys(Keys.RETURN)
 
-        # Search for Locofy.ai
-        plugin_option = driver.find_element(By.XPATH, "//div[text()='Locofy Lightning - Figma to Code in a flash']")
-        plugin_option.click()
-        time.sleep(5)  # Wait for the plugin to load
 
         # click on run button
-        driver.execute_script('document.querySelector("#react-page > div > div > div > div.fullscreen_view--flexContainer--SBt2E > div:nth-child(1) > div > div > div.utils-module--contents--iP8Hi > div > div > div > div > div > div.cx_flex--2hUIC.cx_flexRow--QnXvr.cx_gap8--RCB1-.cx_justifyBetween--ihJXN.cx_pt8--kmTV-.cx_pb8--v4HS8.cx_bSolid--6-DiT.cx_colorBorder--W0gXm.cx_bt1--nb1cc.cx_itemsCenter--4Ybl3.cx_colorBg--XYMUF > div > button.button-reset-module--buttonReset--Njg3k.button-module--button--xiqeQ.utilities--bodyMedium--7-YGK.button-module--regularSize--Mmmf7.button-module--primary--pCfBo.button-module--solidStyle--mSysv").click()')
-        time.sleep(20)
+        wait_for_script(driver, 'document.querySelector("#react-page > div > div > div > div.fullscreen_view--flexContainer--SBt2E > div:nth-child(1) > div > div > div.utils-module--contents--iP8Hi > div > div > div > div > div > div.cx_flex--2hUIC.cx_flexRow--QnXvr.cx_gap8--RCB1-.cx_justifyBetween--ihJXN.cx_pt8--kmTV-.cx_pb8--v4HS8.cx_bSolid--6-DiT.cx_colorBorder--W0gXm.cx_bt1--nb1cc.cx_itemsCenter--4Ybl3.cx_colorBg--XYMUF > div > button.button-reset-module--buttonReset--Njg3k.button-module--button--xiqeQ.utilities--bodyMedium--7-YGK.button-module--regularSize--Mmmf7.button-module--primary--pCfBo.button-module--solidStyle--mSysv").click()') 
 
-        iframe = driver.find_element(By.ID, 'plugin-iframe-in-modal')  
+        iframe = wait_element(driver, By.ID, "plugin-iframe-in-modal", timeout=30)
         driver.switch_to.frame(iframe)
 
-        f1 = driver.find_element(By.NAME, "Network Plugin Iframe")
+        f1 = wait_element(driver, By.NAME, "Network Plugin Iframe")
         driver.switch_to.frame(f1)
-        
+
         # Switch to the inner iframe
-        inner0_iframe = driver.find_element(By.ID, "plugin-iframe")
+        inner0_iframe = wait_element(driver, By.ID, "plugin-iframe")
         driver.switch_to.frame(inner0_iframe)
-        driver.execute_script('document.querySelector("._btn_1telm_80").click()')
-        time.sleep(1)
-        
+        time.sleep(0.5)
+        wait_for_script(driver, 'document.querySelector("._btn_1telm_80").click()')
+        time.sleep(0.5)
+
         # Switch to the new tab
         driver.switch_to.window(driver.window_handles[1])
-        time.sleep(5)
 
         # Retrieve Locofy credentials from environment variables
         # Login to locofy
 
         # Locate the email and password fields within the iframe
-        email_field = driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div/div/div/div[2]/div[1]/input')
-        password_field = driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div/div/div/div[2]/div[2]/input')
+        email_field = wait_element(driver, By.XPATH, '//*[@id="app"]/div/div/div/div/div/div[2]/div[1]/input')
+        password_field = wait_element(driver, By.XPATH, '//*[@id="app"]/div/div/div/div/div/div[2]/div[2]/input')
 
         # Enter the credentials
         email_field.send_keys(LOCOFY_EMAIL)
         password_field.send_keys(LOCOFY_PASSWORD)
 
         # Click the login button
-        login_button = driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div/div/div/div[2]/button')
+        login_button = wait_element(driver, By.XPATH, '//*[@id="app"]/div/div/div/div/div/div[2]/button')
         login_button.click()
 
-        # use google login instead
-        # Wait for the login to complete
-        time.sleep(10)
         driver.switch_to.window(driver.window_handles[0])
+        time.sleep(2)
         # gen code button
-        iframe = driver.find_element(By.ID, 'plugin-iframe-in-modal')
+        iframe = wait_element(driver, By.ID, 'plugin-iframe-in-modal')
         driver.switch_to.frame(iframe)
 
-        f1 = driver.find_element(By.NAME, "Network Plugin Iframe")
+        f1 = wait_element(driver, By.NAME, "Network Plugin Iframe")
         driver.switch_to.frame(f1)
-        time.sleep(5)
+
         # Switch to the inner iframe
-        inner0_iframe = driver.find_element(By.ID, "plugin-iframe")
-        time.sleep(2)
+        inner0_iframe = wait_element(driver, By.ID, "plugin-iframe")
         driver.switch_to.frame(inner0_iframe)
         time.sleep(2)
-        try:
-            driver.execute_script('document.querySelectorAll(".font-medium")[1].click()')
-            time.sleep(1)
-        except Exception as e:
-            print(e)
-        driver.execute_script('document.querySelector("._shinyBtn_1lwba_6").click()')
-        time.sleep(1)
 
+        wait_for_script(driver, 'document.querySelectorAll(".font-medium")[1].click()')
+        time.sleep(2)
+        wait_for_script(driver, 'document.querySelector("._shinyBtn_1lwba_6").click()')
+        time.sleep(2)
         # select frame 1
-        driver.execute_script('document.querySelector("._frameImage_1ip34_91").click()')
+        wait_for_script(driver, 'document.querySelector("._frameImage_1ip34_91").click()')
         time.sleep(2)
         # click to convert 1 frame _shinyBtn_1lwba_6
-        driver.execute_script('document.querySelector("._shinyBtn_1lwba_6").click()')
-        time.sleep(1)
+        wait_for_script(driver, 'document.querySelector("._shinyBtn_1lwba_6").click()')
+        time.sleep(2)
         # sleep 60 seconds
-        time.sleep(30)
-        def wait_element(xpath, timeout=10):
-            return WebDriverWait(driver, timeout).until(EC.element_to_be_clickable((By.XPATH, xpath)))
-
-        wait_element('//*[@id="root-preview"]/div[1]/div[3]/div[5]/button').click()
-        wait_element('//*[@id="portal"]/div[6]/div[2]/div/div/div[2]/div[1]/span/span/div/div').click()
-        wait_element('//*[@id="portal"]/div[6]/div[2]/div/div[2]/div/div[2]/div/div[1]').click()
-        wait_element('//*[@id="portal"]/div[6]/div[2]/div/div[3]/div/div/span/span/button').click()
-        time.sleep(5)
-        wait_element('//*[@id="portal"]/div[6]/div[2]/div[2]/div[2]/button').click()
+        time.sleep(10)
+        wait_element(driver, By.XPATH, '//*[@id="root-preview"]/div[1]/div[3]/div[5]/button',timeout=50).click()
+        wait_element(driver, By.XPATH, '//*[@id="portal"]/div[6]/div[2]/div/div/div[2]/div[1]/span/span/div/div').click()
+        time.sleep(1)
+        wait_element(driver, By.XPATH, '//*[@id="portal"]/div[6]/div[2]/div/div[2]/div/div[2]/div/div[1]').click()
+        wait_element(driver, By.XPATH, '//*[@id="portal"]/div[6]/div[2]/div/div[3]/div/div/span/span/button').click()
+        wait_element(driver, By.XPATH, '//*[@id="portal"]/div[6]/div[2]/div[2]/div[2]/button').click()
         driver.switch_to.window(driver.window_handles[2])
-        wait_element('//*[@id="app"]/div[2]/div/div[3]/button[1]').click()
+        time.sleep(1)
+        wait_element(driver, By.XPATH, '//*[@id="app"]/div[2]/div/div[3]/button[1]').click()
+
         try:
-            wait_element('//*[@id="portal"]/div[20]/div/div[2]/div/div/img[3]').click()
+            wait_element(driver, By.XPATH, '//*[@id="portal"]/div[19]/div/div[2]/div/div/img[3]').click()
             time.sleep(1)
         except Exception as e:
+            print("Error in click on image x")
             print(e)
-        wait_element('//*[@id="header"]/div[3]/div/div/button').click()
-        wait_element('//*[@id="portal"]/div[21]/div/div[3]/div[3]').click()
-        wait_element('//*[@id="portal"]/div[22]/div/div[2]/div/div/div[2]/div[3]/div[2]/div/div/div/button').click()
-
-
+        
+        wait_element(driver, By.XPATH, '//*[@id="header"]/div[3]/div/div/button').click()
+        time.sleep(0.5)
+        wait_element(driver,By.XPATH, "//div[text()='Export Project']").click()
+        time.sleep(0.5)
+        wait_element(driver, By.XPATH, '//*[@id="portal"]/div[22]/div/div[2]/div/div/div[2]/div[3]/div[2]/div/div/div/button').click()
+        time.sleep(0.5)
+    except Exception as e:
+        print("Error in automate_locofyai")
+        print(traceback.format_exc())
+        print(e)
     finally:
         # Close the browser
         driver.quit()
